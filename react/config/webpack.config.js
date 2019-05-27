ae48a2d0e308d1da7d3b1c4418acb8fe2ce24fe5
liutao <painterpuppets@gmail.com>
2019-05-27 18:47:27 +0800

    fix docker build

--- react/config/webpack.config.js
+++ react/config/webpack.config.js
@@ -23,7 +23,7 @@ const getClientEnvironment = require('./env');
 const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin-alt');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
 // Source maps are resource heavy and can cause out of memory issue for large source files.
@@ -499,7 +499,7 @@ module.exports = function () {
       ],
     },
     plugins: [
-      new BundleAnalyzerPlugin(),
+      // new BundleAnalyzerPlugin(),
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
