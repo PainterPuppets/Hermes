73547febab238619e76391bb9ac8dee4303b54fd
PainterPuppets <painterpppets@gmail.com>
2019-06-03 10:54:43 +0800

    remove guild

--- guild/models.py
+++ guild/models.py
@@ -1,22 +1,22 @@
-# coding: utf-8
-import json
+# # coding: utf-8
+# import json
 
-from django.db import models
-from django.contrib.auth.models import User
-from message.models import Message, Channel
+# from django.db import models
+# from django.contrib.auth.models import User
+# from message.models import Message, Channel
 
-class Guild(models.Model):
-    name = models.TextField()
-    avatar = models.ImageField(blank=True, upload_to="avatars/")
-    creator = models.ForeignKey(User, on_delete=None, related_name="create_guilds")
-    members = models.ManyToManyField(User, related_name="join_guilds")
-    created_at = models.DateTimeField(auto_now_add=True)
+# class Guild(models.Model):
+#     name = models.TextField()
+#     avatar = models.ImageField(blank=True, upload_to="avatars/")
+#     creator = models.ForeignKey(User, on_delete=None, related_name="create_guilds")
+#     members = models.ManyToManyField(User, related_name="join_guilds")
+#     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Role(models.Model):
-    name = models.TextField()
-    guild = models.ForeignKey(Guild, on_delete=None)
-    color = models.TextField()
+# class Role(models.Model):
+#     name = models.TextField()
+#     guild = models.ForeignKey(Guild, on_delete=None)
+#     color = models.TextField()
 
 
 # class Channel(models.Model):
@@ -24,7 +24,7 @@ class Role(models.Model):
 #     messages = models.ManyToManyField(Message, blank=True)
 
     
-class Categorie(models.Model):
-    name = models.TextField()
-    guild = models.ForeignKey(Guild, on_delete=None)
-    channels = models.ManyToManyField(Channel)
+# class Categorie(models.Model):
+#     name = models.TextField()
+#     guild = models.ForeignKey(Guild, on_delete=None)
+#     channels = models.ManyToManyField(Channel)
